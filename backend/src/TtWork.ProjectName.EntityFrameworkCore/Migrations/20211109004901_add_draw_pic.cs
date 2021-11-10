87b9dd76b802ea040221c040abf1a19b50e9d7ab
TT <tt-tjw@qq.com>
2021-11-10 11:30:02 +0800

    save

---
+++ backend/src/TtWork.ProjectName.EntityFrameworkCore/Migrations/20211109004901_add_draw_pic.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TtWork.ProjectName.Migrations
+{
+    public partial class add_draw_pic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TitleImageUrl",
+                table: "Activity_LuckDraws",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleImageUrl",
+                table: "Activity_LuckDraws");
+        }
+    }
+}
