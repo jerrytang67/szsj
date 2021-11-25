f2d1f15a45bbd2b54c85d61b04e76531fa8efd9b
TT <tt-tjw@qq.com>
2021-11-25 21:11:03 +0800

    save

---
+++ backend/Modules/TTWork.Abp.Core/Extensions/JsonExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Abp.Json;
+using Newtonsoft.Json;
+
+namespace SiVan.Abp.Core.Extensions
+{
+    public static class JsonExtensionsExt
+    {
+        public static T FromJsonStringExt<T>(this string value)
+        {
+            try
+            {
+                return value.FromJsonString<T>(new JsonSerializerSettings());
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+        }
+    }
+}
