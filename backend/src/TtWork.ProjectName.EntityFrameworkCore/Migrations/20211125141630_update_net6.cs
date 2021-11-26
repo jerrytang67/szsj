85d97c76b4111dc2aaa46451f4d8b7f439cd4c83
TT <tt-tjw@qq.com>
2021-11-26 11:51:50 +0800

    save

---
+++ backend/src/TtWork.ProjectName.EntityFrameworkCore/Migrations/20211125141630_update_net6.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TtWork.ProjectName.Migrations
+{
+    public partial class update_net6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AbpUserLogins_ProviderKey_TenantId",
+                table: "AbpUserLogins",
+                columns: new[] { "ProviderKey", "TenantId" },
+                unique: true,
+                filter: "[TenantId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AbpUserLogins_ProviderKey_TenantId",
+                table: "AbpUserLogins");
+        }
+    }
+}
