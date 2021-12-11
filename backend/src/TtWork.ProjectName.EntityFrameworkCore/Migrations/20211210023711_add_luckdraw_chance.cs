8bfeaf3e77aa04a89ba6b13855ce1fc573cff626
TT <tt-tjw@qq.com>
2021-12-11 10:02:31 +0800

    默认中率

---
+++ backend/src/TtWork.ProjectName.EntityFrameworkCore/Migrations/20211210023711_add_luckdraw_chance.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TtWork.ProjectName.Migrations
+{
+    public partial class add_luckdraw_chance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DefaultWinningChance",
+                table: "Activity_LuckDraws",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultWinningChance",
+                table: "Activity_LuckDraws");
+        }
+    }
+}
